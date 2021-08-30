1066b6f84ae0d25a2c3d85bf1ac88ba7b6e7ad38
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-08-30 13:04:40 +0200

    Add start scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import * as sourceMaps from "source-map-support";
+import readline from "readline";
+import { calculateScore } from "./calculateScore";
 
 sourceMaps.install();
+
+const lineReader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+lineReader.question("Provide a bowling game like so '12 x 4/ -/ - x x x 5/ -9'\n", (input) => {
+  console.log(`The total score is ${calculateScore(input)}`);
+  lineReader.close();
+});
